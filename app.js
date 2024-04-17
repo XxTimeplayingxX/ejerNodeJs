8c64e9735924b3fef7242a45b3983cf148f81639
David <david.sanchez@tecnologicoliceocristiano.edu.ec>
2024-04-16 23:05:54 -0500

    punto extra

---
+++ app.js
@@ -0,0 +1,19 @@
+// Una libería para poder acceder a los archivos del sistema
+//Hacemos una instancia de una ibrería
+const fs = require('fs');
+
+const userName = 'David';
+const lastName = 'Sánchez'
+
+fs.writeFile('user-data.txt', 'Nombre: '+userName +'\nApellido: ' +lastName, (err)=>{
+    if(erro =  null){
+        console.log(err);
+        return
+    }
+    console.log('Hackeando NASA 20%');
+    console.log('Hackeando NASA 40%');
+    console.log('Hackeando NASA 60%');
+    console.log('Hackeando NASA 80%');
+    console.log('Hackeando NASA 100%');
+    console.log('Felicidades ha creado un archivo')
+});
